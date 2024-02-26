bce7fb77db45772a3f90f873004eec98238a51cf
Mr.Irra <74286292+IsrNY@users.noreply.github.com>
2024-02-26 17:56:18 -0600

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bases';
+  public mensaje:string = 'What´s Up';
+
+  public counter:number = 10;
+
+  sumar():void {
+
+    if(this.counter < 20){
+      this.counter += 1;
+    } else {
+      alert('Contador excedido, decrementar')
+    }
+  }
+  restar():void{
+    if(this.counter > 0)
+    {
+    this.counter -= 1;
+    }
+  else {
+    alert('Contador llegó a 0, incrementar')
+  }
+}
+  reset():void{
+    this.counter = 10;
+  }
 }
